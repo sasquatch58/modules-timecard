b18baec3193a869d73fd7dc5175b9f3109ef689a
Keith Casey <keith@caseysoftware.com>
2014-04-20 19:58:11 -0500

    fixing the missing field on the new log entry

--- vw_newlog.php
+++ vw_newlog.php
@@ -432,7 +432,9 @@ $(document).ready(function() {
 <tr>
     <td align="right" valign="middle"><?php echo ($canEditTask ? $AppUI->_('Progress').' : ' : ''); ?></td>
     <td valign="middle">
-        <?php echo ($canEditTask ? arraySelect($percent, 'task_percent_complete', 'size="1" class="text"', $task["task_percent_complete"]) . '%' : '<input type="hidden" name="task_percent_complete" value="0" />');?>
+        <?php
+        $percent = array(5 => 5, 10 => 10, 15 => 15, 20 => 20, 25 => 25, 30 => 30, 35 => 35, 40 => 40, 45 => 45, 50 => 50, 55 => 55, 60 => 60, 65 => 65, 70 => 70, 75 => 75, 80 => 80, 85 => 85, 90 => 90, 95 => 95, 100 => 100);
+        echo ($canEditTask ? arraySelect($percent, 'task_percent_complete', 'size="1" class="text"', $task["task_percent_complete"]) . '%' : '<input type="hidden" name="task_percent_complete" value="0" />');?>
     </td>
 </tr>
 <tr>
